ce405928d7c26dc8846e6a744d1fbc133ed679d3
Shashwat Tiwari <shivajidubey@Shashwats-MacBook-Air.local>
2024-08-31 17:12:15 +0530

    second

--- api/index.js
+++ api/index.js
@@ -35,11 +35,15 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
+
+app.get("/", (req, res) => {
+  res.send("Bakckend is Running not to Worry :)")
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
